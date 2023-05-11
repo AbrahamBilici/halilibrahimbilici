6de530979e76ab182f88cc2c8b19bd83dc100e13
Halil Ibrahim Bilici <115592073+AbrahamBilici@users.noreply.github.com>
2023-05-11 10:57:52 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,97 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/contact.html', (req, res) => {
+    const filePath = path.join(__dirname, 'contact.html');
+    res.sendFile(filePath);
+});
+
+
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.MONGO_URI);
+
+// schema
+
+const contactSchema = new mongoose.Schema({
+    personal_info:
+    {
+        first_name: { type: String, required: true },
+        last_name: { type: String, required: true },
+        email: { type: String, required: true },
+        age_decade: Number,
+        lang: String,
+        location: String
+    }
+    ,
+    region: String,
+    message: String
+});
+
+const User = mongoose.model('User', contactSchema);
+
+// post
+
+app.post('/submit-form', async (req, res) => {
+    const { firstName, lastName, email, decade, language, residence, continent, message } = req.body;
+
+
+
+    // Example: Logging the form data
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('Email:', email);
+    console.log('Decade:', decade);
+    console.log('Language:', language);
+    console.log('Residence:', residence);
+    console.log('Continent:', continent);
+    console.log('Message:', message);
+    const personalInfo = {
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        age_decade: decade,
+        lang: language,
+        location: residence
+    };
+
+
+    try {
+        const user = new User({
+            personal_info: personalInfo,
+            region: continent,
+            message: message
+        });
+        await user.save();
+        // Send a response to the client
+        res.json({ success: true });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Server error' })
+    }
+
+
+});
+
+
+const listener = app.listen(port, () => {
+    console.log('Your app is listening on port ' + listener.address().port)
+})
