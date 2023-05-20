cff3fe883d53b8f926277ae020499d72a5f601ed
Halil Ibrahim Bilici <halilibrahimbilici@hotmail.com>
2023-05-19 20:28:56 -0600

    fix: path was with mistake , added / to filepath

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/contact.html', (req, res) => {
-    const filePath = __dirname + 'contact.html';
+    const filePath = __dirname + '/contact.html';
     res.sendFile(filePath);
 });
 
