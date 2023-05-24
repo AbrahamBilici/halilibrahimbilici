77a19a78cfdd5ecf4e8e3dfc031d6d2e725b19dc
Halil Ibrahim Bilici <halilibrahimbilici@hotmail.com>
2023-05-24 10:55:35 -0600

    fix: updated node.js.yml

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 27017;
+const port = process.env.PORT;
 
 
 app.use(cors());
